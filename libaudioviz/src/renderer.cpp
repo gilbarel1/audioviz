688281eb5a3826e644b57a0ad50f0b6bd7f66690
user <hadadim@post.bgu.ac.il>
2025-12-26 19:24:07 +0200

    using cli stft implementation instead of prev random impl in main, modified cli to use renderer

--- libaudioviz/src/renderer.cpp
+++ libaudioviz/src/renderer.cpp
@@ -16,13 +16,10 @@ void Renderer::initialize_window() {
     std::cout << "Window initialized" << std::endl;
 }
 
-/*void Renderer::update_data(std::complex<float>* data, size_t size) {
-    // verify data transfer
-    if (size > 0) {
-         std::cout << "Received buffer. First sample: " << data[0] << std::endl;
-    }
-}*/
 
 void Renderer::render_frame(std::complex<float>* data, size_t size) {
     std::cout << "Frame rendered" << std::endl;
+    if (size > 0) {
+        std::cout << "Data[0]: " << data[0] << " | Size: " << size << std::endl;
+    } 
 }
